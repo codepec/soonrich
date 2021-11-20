50cca2da4ceb7b6af091466554231a241ae0643d
codepec <christoph.peschke@gmail.com>
2021-11-20 20:48:07 +0100

    add some new files

---
+++ script.js
@@ -0,0 +1,22 @@
+function ticken(){
+  var stunden, minuten, sekunden;
+  var StundenZahl, MinutenZahl, SekundenZahl;
+  var heute;
+
+  heute = new Date();
+  StundenZahl = heute.getHours();
+  MinutenZahl = heute.getMinutes();
+  SekundenZahl = heute.getSeconds();
+
+  stunden = StundenZahl+":";
+  if (MinutenZahl < 10) {minuten = "0" + MinutenZahl + ":";}
+    else {minuten = MinutenZahl + ":";}
+  if (SekundenZahl < 10) {sekunden = "0" + SekundenZahl + " ";}
+    else {sekunden = SekundenZahl + " ";}
+  zeit = stunden + minuten + sekunden + " Uhr";
+  uhr.innerHTML = zeit;
+
+  window.setTimeout("ticken();", 1000);
+}
+
+window.onload = ticken;
